84d176a384252a4af9cfe0449297af7f9e33a77e
Ron Hershkovitz <ron9hm1@gmail.com>
2020-02-12 12:03:19 +0200

    yesh text

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -6,6 +6,10 @@ var gCanvas;
 var gCtx;
 var gChangeCanvas = false;
 
+// text variables
+
+var elText1 = document.querySelector('.text1')
+
 function onInit() {
     gImgs = getImgsToShow();
     gCanvas = document.querySelector('#my-canvas');
@@ -14,11 +18,11 @@ function onInit() {
     resizeCanvas();
     // TODO : addEventListeners() for fluid sensitivity
 }
-function renderImgs(){
+function renderImgs() {
     let elImgs = document.querySelector('.imgs');
     let strHTMLS = '';
-    gImgs.forEach((img,idx) => {
-        strHTMLS+=  `<img class="img-${idx+1}" src="imgs/${idx+1}.jpg " alt="" onclick="openMemeEditor(this)"></img>`
+    gImgs.forEach((img, idx) => {
+        strHTMLS += `<img class="img-${idx + 1}" src="imgs/${idx + 1}.jpg " alt="" onclick="openMemeEditor(this)"></img>`
     });
     elImgs.innerHTML = strHTMLS;
 }
@@ -29,24 +33,29 @@ function resizeCanvas() {
     gCanvas.height = elContainer.offsetHeight;
 }
 
-function openMemeEditor(currImg){
+function openMemeEditor(currImg) {
     var elContainer = document.querySelector('.canvas-container');
     // elContainer.removeAttribute("hidden")
     saveCurrImgToService(currImg)
     printImgOnCanvas();
-    gChangeCanvas =true;
+    
 }
-function printImgOnCanvas(){
-  let selectedImgUrl =  getCurrImgFromService()
+function printImgOnCanvas() {
+    let selectedImgUrl = getCurrImgFromService()
     var img = new Image()
     img.src = selectedImgUrl;
     img.onload = () => {
         gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
+        if(gChangeCanvas){
+            
+            drawText(elText1.value, 0, 0)
+        }gChangeCanvas = true;
     }
 }
 
-function onChangeText(){
-get
+function onChangeText() {
+    debugger
+    printImgOnCanvas()    
 }
 
 function drawText(text, x, y) {
@@ -55,6 +64,6 @@ function drawText(text, x, y) {
     // gCtx.fillStyle = 'white'
     gCtx.font = '40px Ariel'
     // gCtx.textAlign = 'center'
-    gCtx.fillText(text, x, y)
-    gCtx.strokeText(text, x, y)
+    gCtx.fillText(text, 30, 40)
+    gCtx.strokeText(text, 30, 40)
 }
