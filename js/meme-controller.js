8d6ee66c5d999c4b7391d92cc06046cd4e696329
Ron Hershkovitz <ron9hm1@gmail.com>
2020-02-12 13:52:09 +0200

    before addint more txt

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -4,16 +4,19 @@ var gImgs;
 // canvas variables
 var gCanvas;
 var gCtx;
-var gChangeCanvas = false;
+// var gChangeCanvas = false;
 
 // text variables
 
 var elText1 = document.querySelector('.text1')
+var elText2 = document.querySelector('.text2')
+var gSelectedTextIndx = 0;
 
 function onInit() {
     gImgs = getImgsToShow();
     gCanvas = document.querySelector('#my-canvas');
     gCtx = gCanvas.getContext('2d');
+    addEventListeners()
     renderImgs();
     resizeCanvas();
     // TODO : addEventListeners() for fluid sensitivity
@@ -22,7 +25,7 @@ function renderImgs() {
     let elImgs = document.querySelector('.imgs');
     let strHTMLS = '';
     gImgs.forEach((img, idx) => {
-        strHTMLS += `<img class="img-${idx + 1}" src="imgs/${idx + 1}.jpg " alt="" onclick="openMemeEditor(this)"></img>`
+        strHTMLS += `<img class="img img-${idx + 1}" src="imgs/${idx + 1}.jpg " alt="" onclick="openMemeEditor(this)"></img>`
     });
     elImgs.innerHTML = strHTMLS;
 }
@@ -38,32 +41,79 @@ function openMemeEditor(currImg) {
     // elContainer.removeAttribute("hidden")
     saveCurrImgToService(currImg)
     printImgOnCanvas();
-    
+
 }
 function printImgOnCanvas() {
-    let selectedImgUrl = getCurrImgFromService()
+    let selectedImgUrl = getCurrImgFromService().url
     var img = new Image()
     img.src = selectedImgUrl;
     img.onload = () => {
         gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
-        if(gChangeCanvas){
-            
-            drawText(elText1.value, 0, 0)
-        }gChangeCanvas = true;
+        // if (gChangeCanvas) {
+            drawRect()
+            drawText(elText1.value)
+
+            drawRect()
+            drawText(elText2.value)
+        // } gChangeCanvas = true;
     }
 }
 
 function onChangeText() {
-    debugger
-    printImgOnCanvas()    
+    printImgOnCanvas()
 }
 
-function drawText(text, x, y) {
+function drawText(text , x, y) {
+    if(!text){
+        text = getModelText()
+    }
+    let txtCoords = getTextCoords()
     // gCtx.lineWidth = '2'
     // gCtx.strokeStyle = 'red'
     // gCtx.fillStyle = 'white'
     gCtx.font = '40px Ariel'
     // gCtx.textAlign = 'center'
-    gCtx.fillText(text, 30, 40)
-    gCtx.strokeText(text, 30, 40)
+    gCtx.fillStyle = 'black'
+    gCtx.fillText(text, txtCoords[0], txtCoords[1])
+    gCtx.strokeText(text, txtCoords[0], txtCoords[1])
+    changeModelText(text,gSelectedTextIndx)
+}
+function drawRect(x, y) {
+    let txtCoords = getTextCoords()
+    gCtx.beginPath()
+    gCtx.fillStyle = 'rgba(182, 182, 182, 0.699)'
+    gCtx.fillRect(0, txtCoords[1]-40, gCanvas.width, 100)
+}
+
+function addEventListeners() {
+    gCanvas.addEventListener('mousedown', e => {
+        let xy = [e.clientX,e.clientY];
+        sendCoordsToModel(xy)
+        printImgOnCanvas()
+    });
+
+    // gCanvas.addEventListener('mousemove', e => {
+    //     if (isDrawing === true) {
+    //         console.log(e)
+    //         var mousex = e.pageX;
+    //         var mousey = e.pageY;
+    //         if (gLastMouseX > -1)
+    //             gMouseTravel = Math.max(Math.abs(mousex - gLastMouseX), Math.abs(mousey - gLastMouseY));
+    //         gLastMouseX = mousex;
+    //         gLastMouseY = mousey;
+    //         draw(x, y, e.clientX, e.clientY);
+    //         x = e.clientX;
+    //         y = e.clientY;
+    //     }
+    // });
+
+    // window.addEventListener('mouseup', e => {
+    //     if (isDrawing === true) {
+    //         resetMouseSpeed()
+    //         draw(x, y, e.clientX, e.clientY);
+    //         x = 0;
+    //         y = 0;
+    //         isDrawing = false;
+    //     }
+    // });
 }
--- js/meme-service.js
+++ js/meme-service.js
@@ -1,7 +1,23 @@
 var gKeywords = { 'happy': 12, 'funny puk': 1 }
+
 var gImgs = [{ id: 1, url: 'imgs/1.jpg', keywords: ['happy'] },
-{ id: 2, url: 'imgs/2.jpg', keywords: ['happy'] }
+{ id: 2, url: 'imgs/2.jpg', keywords: ['happy'] },
+{ id: 3, url: 'imgs/3.jpg', keywords: ['happy'] },
+{ id: 4, url: 'imgs/4.jpg', keywords: ['happy'] },
+{ id: 5, url: 'imgs/5.jpg', keywords: ['happy'] },
+{ id: 6, url: 'imgs/6.jpg', keywords: ['happy'] },
+{ id: 7, url: 'imgs/7.jpg', keywords: ['happy'] },
+{ id: 8, url: 'imgs/8.jpg', keywords: ['happy'] },
+{ id: 9, url: 'imgs/9.jpg', keywords: ['happy'] },
+{ id: 10, url: 'imgs/10.jpg', keywords: ['happy'] },
+{ id: 11, url: 'imgs/11.jpg', keywords: ['happy'] },
+{ id: 12, url: 'imgs/12.jpg', keywords: ['happy'] },
+{ id: 13, url: 'imgs/13.jpg', keywords: ['happy'] },
+{ id: 14, url: 'imgs/14.jpg', keywords: ['happy'] },
+{ id: 15, url: 'imgs/15.jpg', keywords: ['happy'] },
+{ id: 16, url: 'imgs/16.jpg', keywords: ['happy'] },
 ];
+
 var gMeme = {
     selectedImgId: 5,
     selectedLineIdx: 0,
@@ -10,22 +26,30 @@ var gMeme = {
             txt: 'I never eat Falafel',
             size: 20,
             align: 'left',
-            color: 'red'
+            color: 'red',
+            xy: [30, 70]
         }, {
-            txt: 'I never eat Falafel',
+            txt: 'but I eat Mafrum ',
             size: 20,
             align: 'left',
-            color: 'red'
+            color: 'red',
+            xy: [30, 200]
         }
     ]
 }
-var gImgIndex = -Infinity
-
+function getTextCoords() {
+    return gMeme.lines[gMeme.selectedLineIdx].xy;
+}
 // imgs
 
 function createImgs() {
     // TODO: get imgs for gImgs from folder
-    createImgs()
+    for (let x = 0; x < 18; x++) {
+        gImgs.push(createImg())
+    }
+}
+function createImg() {
+    var img = {}
 }
 
 function getImgsToShow() {
@@ -33,10 +57,19 @@ function getImgsToShow() {
 }
 
 function saveCurrImgToService(currImg) {
-    debugger
-    gImgIndex = (currImg.classList.value)
-    gImgIndex = +(gImgIndex.split("-"))[1] - 1
+    var ImgIndex = (currImg.classList.value)
+    gMeme.selectedImgId = +(ImgIndex.split("-"))[1] - 1
 }
 function getCurrImgFromService() {
-    return gImgs[gImgIndex].url
+    return gImgs[gMeme.selectedImgId]
+}
+function changeModelText(text, selectedTextIndx) {
+    gMeme.lines[selectedTextIndx].txt = text
+    gMeme.selectedLineIdx = selectedTextIndx
+}
+function getModelText() {
+    return gMeme.lines[gMeme.selectedLineIdx].txt
+}
+function sendCoordsToModel(xy) {
+    gMeme.lines[gMeme.selectedLineIdx].xy = xy;
 }
